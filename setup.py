89e94d46f68bfc2e266567a0bb8eb426049765e2
yoyonel <yoyonel@hotmail.com>
2018-01-21 22:50:29 +0100

    refactor a bit, pycharm.

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ for line in open('./lib/version.py'):
 else:
     raise Exception("Could not find package version")
 
-
 setup(
     name='cbktree',
     version=version,
@@ -19,16 +18,23 @@ setup(
     package_dir={"cbktree": "lib"},
     maintainer="Guilherme Polo",
     url="https://github.com/gpip/cBKTree",
-    ext_modules=cythonize([Extension(
-        "cbktree/cyHamDb",
-        sources=["lib/cyHamDb.pyx"],
-        libraries=["stdc++"],
-        include_dirs=['lib'],
-        language="c++",
-        extra_compile_args=["-std=c++11", "-march=native", "-mtune=native", "-O3", "-mpopcnt"],
-        extra_link_args=["-std=c++11", "-march=native", "-mtune=native", "-O3", "-mpopcnt"]
-    )]),
-    install_requires=['Cython'],
+    ext_modules=cythonize(
+        [
+            Extension(
+                "cbktree/cyHamDb",
+                sources=["lib/cyHamDb.pyx"],
+                libraries=["stdc++"],
+                include_dirs=['lib'],
+                language="c++",
+                extra_compile_args=["-std=c++11", "-march=native", "-mtune=native", "-O3", "-mpopcnt"],
+                extra_link_args=["-std=c++11", "-march=native", "-mtune=native", "-O3", "-mpopcnt"]
+            )
+        ],
+        gdb_debug=True
+    ),
+    install_requires=[
+        'Cython'
+    ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
 )
